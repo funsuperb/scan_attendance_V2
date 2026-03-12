16a3cd8ba7beecf9282f66936a536194c0711e81
funsuperb <fstromey@gmail.com>
2026-03-12 11:17:46 +0700

    Add files via upload

---
+++ js/api-config.js
@@ -0,0 +1,5 @@
+// ============================================================
+//  ตั้งค่า Google Apps Script Web App URL
+//  แก้ไข URL ด้านล่างหากมีการ Deploy GAS ใหม่
+// ============================================================
+const GAS_API_URL = 'https://script.google.com/macros/s/AKfycbyX2BPx8J5Bk3lxFE-9WrK-pXZ56fivwc4wbxqA-LY4P0TTZ2h9fuaeBVc96ljU6KA/exec';
